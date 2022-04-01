9c4af90ee923f6a6fe05980c13eae3f8aceec075
sharpe-byte <harp6163@gmail.com>
2022-03-31 19:05:27 -0500

    Initial commit of new version. Replaced images with .webp versions + added preloader and fade-in animation when website loads + organized code + deleted unused images + changed layout of gallery in gallery.html and kind of made it responsive + added some content to events.html such as a section with background image + changed background images for other html files except for index.html by using the newly added hero-image files + styling

---
+++ README.md
@@ -0,0 +1,2 @@
+# nlrhs-anime-club-website
+Website for the Anime Club in North Little Rock High School
---
+++ js/script.js
@@ -0,0 +1,52 @@
+// Preloader setup
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {
+    showPage();
+  }
+};
+
+function showPage() {
+  document.getElementById("page-content").style.display = "block";
+  document.getElementById("preloader").style.display = "none";
+}
+
+// Disable selectiong
+document.onselectstart = () => {
+  return false;
+};
+
+// Disable drag
+document.ondragstart = () => {
+  return false;
+};
+
+// Set copyright
+window.onload = () => {
+  const year = new Date().getFullYear();
+  document.querySelector(
+    "#page-content > footer > div.copyright > p"
+  ).textContent = `© Copyright ${year}, NLRHS Anime Club. All Rights Reserved.`;
+};
+
+// Header background color setup
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+function changeHeaderBackgroundColor(scrollPos) {
+  if (scrollPos <= 30) {
+    document.querySelector("header").style.backgroundColor = "transparent";
+  } else {
+    document.querySelector("header").style.backgroundColor = "black";
+  }
+}
+
+document.addEventListener("scroll", function (e) {
+  lastKnownScrollPosition = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(function () {
+      changeHeaderBackgroundColor(lastKnownScrollPosition);
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
